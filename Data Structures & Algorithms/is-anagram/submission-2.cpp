c6f7816217f6e1b1d3e3a01d8e2b9e06f0b93b0d
Kunal Khandelwal <132251887+Kunal89204@users.noreply.github.com>
2026-08-31 17:04:55 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        unordered_map<char, int> seen;
+
+        for(int i = 0; i < s.size(); i++){
+            seen[s[i]]++;
+        }
+
+        for(int i = 0; i < t.size(); i++){
+            seen[t[i]]--;
+        }
+
+        for( auto pair: seen){
+            if(pair.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
